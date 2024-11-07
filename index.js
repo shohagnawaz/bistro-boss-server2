68c83ef9345b3eebdcd2fce1f35f0191034d04c9
shohagnawaz <snawazdigital@gmail.com>
2024-11-07 16:50:36 +0600

    reviews

--- index.js
+++ index.js
@@ -25,11 +25,16 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewsCollection = client.db("bistroDb").collection("reviews");
 
     app.get('/menu', async(req, res) => {
         const result = await menuCollection.find().toArray();
         res.send(result);
-    })
+    });
+    app.get('/reviews', async(req, res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
